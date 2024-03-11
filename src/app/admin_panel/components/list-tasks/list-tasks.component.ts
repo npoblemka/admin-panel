cf07bef32743eb070d96dd76c56d12784f8dd0dc
npoblemka <svetlana20694@gmail.com>
2024-03-11 20:38:20 +0300

    create new task component

--- src/app/admin_panel/components/list-tasks/list-tasks.component.ts
+++ src/app/admin_panel/components/list-tasks/list-tasks.component.ts
@@ -1,12 +1,17 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, inject } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { ITask } from '../../../model';
+import { RouterLink } from '@angular/router';
+import {MatDialog,  MatDialogModule} from '@angular/material/dialog';
+import { app } from '../../../../../server';
+import { NewTaskComponent } from '../new-task/new-task.component';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-list-tasks',
@@ -18,11 +23,16 @@ import { ITask } from '../../../model';
       MatSortModule,
       MatPaginatorModule,
       MatButtonModule,
-      MatCardModule],
+      MatCardModule,
+      RouterLink,
+      MatDialogModule,
+      NewTaskComponent,
+      MatNativeDateModule],
   templateUrl: './list-tasks.component.html',
-  styleUrl: './list-tasks.component.scss'
+  styleUrl: './list-tasks.component.scss',
 })
 export class ListTasksComponent {
+  dialog = inject (MatDialog)
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   listTasks!: MatTableDataSource<ITask>;
@@ -39,5 +49,8 @@ export class ListTasksComponent {
     if (this.listTasks.paginator) {
       this.listTasks.paginator.firstPage();
     }
+  }
+  openDialog() {
+    this.dialog.open(NewTaskComponent);
   }
 }
--- src/app/admin_panel/components/new-task/new-task.component.ts
+++ src/app/admin_panel/components/new-task/new-task.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports:
+    [MatDialogModule,
+      MatButtonModule,
+      MatInputModule,
+      MatIconModule,
+      ReactiveFormsModule,
+      MatDatepickerModule,
+      MatNativeDateModule,
+      MatCardModule,
+      MatAutocompleteModule,
+      MatCheckboxModule,
+      MatRadioModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.scss'
 })
 export class NewTaskComponent {
+
+  fb = inject(FormBuilder);
+  myControl = new FormControl('');
+  options: string[] = ['User1', 'User2', 'User3'];
+
+  newTaskForm = this.fb.group({
+    executor: this.myControl,
+    title: this.fb.control('', Validators.required),
+    text: this.fb.control('', Validators.required),
+    didline: this.fb.control(new Date(), Validators.required),
+    priority: this.fb.control('false'),
+    status: this.fb.control('не исполнена'),
+  })
+
+  addNewTask(){
+    if (this.newTaskForm.valid) {
+      console.log(this.newTaskForm)
+    } else {
+      console.log('error')
+    }
+  }
+  removeText() {
+    this.newTaskForm.get('text')?.setValue('');
+  }
+
+ 
 
 }
